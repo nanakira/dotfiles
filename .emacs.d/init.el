3e8d1b545ce7be73090f4605d291a8145078d7c8
nanakira <nanakira@users.noreply.github.com>
2016-05-05 20:55:22 +0900

    Add initial init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,63 @@
+;;; パッケージの設定
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;; 右から左に読む言語に対応させないことで描画高速化
+(setq-default bidi-display-reordering nil)
+
+;;; splash screenを無効にする
+(setq inhibit-splash-screen t)
+
+;;; 同じ内容を履歴に記録しないようにする
+(setq history-delete-duplicates t)
+
+;; C-u C-SPC C-SPC …でどんどん過去のマークを遡る
+(setq set-mark-command-repeat-pop t)
+
+;;; 複数のディレクトリで同じファイル名のファイルを開いたときのバッファ名を調整する
+(require 'uniquify)
+;; filename<dir> 形式のバッファ名にする
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "[^*]+")
+
+;;; ファイルを開いた位置を保存する
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (concat user-emacs-directory "places"))
+
+;;; 釣合う括弧をハイライトする
+(show-paren-mode 1)
+
+;;; インデントにTABを使わないようにする
+(setq-default indent-tabs-mode nil)
+
+;;; 現在行に色をつける
+(global-hl-line-mode 1)
+
+;;; ミニバッファ履歴を次回Emacs起動時にも保存する
+(savehist-mode 1)
+
+;;; シェルに合わせるため、C-hは後退に割り当てる
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;;; モードラインに時刻を表示する
+(display-time)
+
+;;; 行番号・桁番号を表示する
+(line-number-mode 1)
+(column-number-mode 1)
+
+;;; GCを減らして軽くする
+(setq gc-cons-threshold (* 10 gc-cons-threshold))
+
+;;; ログの記録行数を増やす
+(setq message-log-max 10000)
+
+;;; 履歴をたくさん保存する
+(setq history-length 1000)
+
+;;; メニューバーとツールバーを消す
+(menu-bar-mode -1)
+(tool-bar-mode -1)
