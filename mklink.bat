45ab248a5a4e5f0c09239d77b3bcaf39d0f30527
nanakira <nanakira@users.noreply.github.com>
2016-11-20 13:34:19 +0900

    Add .textlintrc

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink %HOME%"\.gitconfig" %HOME%"\dotfiles\.gitconfig"
 mklink %HOME%"\.gitignore" %HOME%"\dotfiles\.gitignore"
 mklink /D %HOME%"\.emacs.d" %HOME%"\dotfiles\.emacs.d"
+mklink %HOME%"\.textlintrc" %HOME%"\dotfiles\.textlintrc"
 exit 0
--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshrc.antigen ~/.zshrc.antigen
 ln -s ~/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/dotfiles/.textlintrc ~/.textlintrc
