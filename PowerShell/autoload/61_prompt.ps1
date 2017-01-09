c6c9531c8422a16d44ab7d38e88e3f011401b169
nanakira <nanakira@users.noreply.github.com>
2017-01-09 14:46:29 +0900

    Update PowerShell prompt

--- PowerShell/autoload/61_prompt.ps1
+++ PowerShell/autoload/61_prompt.ps1
@@ -1,16 +1,37 @@
 # Posh Git prompt setting
 $GitPromptSettings.EnableFileStatus = $true
+$global:GitPromptSettings.BeforeText = '['
+$global:GitPromptSettings.AfterText  = '] '
 
 # Based on poshgit profile.example.ps1
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-
-    Write-Host($pwd.ProviderPath) -nonewline
-
+    $origLASTEXITCODE = $LASTEXITCODE
     Write-VcsStatus
 
-    $global:LASTEXITCODE = $realLASTEXITCODE
+    # Replace $home by ~ if current path includes $home
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+
+    # Limit path length up to maxPathLength
+    $maxPathLength = 40
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.Length -gt $maxPathLength) {
+        $curPath = '...' + $curPath.SubString($curPath.Length - $maxPathLength + 3)
+    }
+
+    Write-Host $curPath -ForegroundColor DarkGreen
+#    Write-Host(Split-Path (Get-Location) -Leaf) -nonewline # only current directory
+    $global:LASTEXITCODE = $origLASTEXITCODE
+
+    if ($?) {
+        Write-Host " (*'-')"  -NoNewLine -ForegroundColor "DarkGreen"
+    } else {
+        Write-Host " (*;-;)"  -NoNewLine -ForegroundColor "Red"
+    }
     return "> "
 }
 
--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,9 +1,9 @@
 # Put PowerShell custom profile directory
 $psdir="$env:HOME\dotfiles\PowerShell"
 
-Write-Host ("Load PS Profiles from {0}\autoload" -f $psdir) -ForegroundColor DarkGreen
+Write-Host ("Load PS Profiles from {0}\autoload" -f $psdir) -ForegroundColor DarkCyan
 
 # Load .ps1 files from "autoload"
 Get-ChildItem $psdir\autoload | where Extension -eq ".ps1" | %{.$_.FullName}
 
-Write-Host "PowerShell Profiles Loaded" -ForegroundColor DarkGreen
+Write-Host "PowerShell Profiles Loaded`n" -ForegroundColor DarkCyan
