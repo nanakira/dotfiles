e9aad12ca2bcabdb7f01a287a3b88f8e30d090a9
nanakira <nanakira@users.noreply.github.com>
2017-07-23 20:39:05 +0900

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshrc.antigen ~/.zshrc.antigen
 ln -s ~/dotfiles/.emacs.d ~/.emacs.d
